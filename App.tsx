d0ad6e80248d51d64befaac91b6b24cf9a540e74
Fruz <faithfulfruz@gmail.com>
2021-08-24 01:32:23 +0100

    Accordian menu transitiion

--- App.tsx
+++ App.tsx
@@ -1,21 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { MutableRefObject, RefObject, useRef, useState } from "react";
+import { StyleSheet, Text, TouchableNativeFeedback, View } from "react-native";
+import {
+  Transition,
+  Transitioning,
+  TransitioningView,
+} from "react-native-reanimated";
 
-export default function App() {
+import data from "./data";
+
+interface Props {
+  transitionRef: RefObject<TransitioningView>;
+}
+
+const transition = (
+  <Transition.Together>
+    <Transition.In type="fade" durationMs={200} />
+    <Transition.Change />
+    <Transition.Out type="fade" durationMs={200} />
+  </Transition.Together>
+);
+
+export default function App({}: Props) {
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const ref = useRef(null) as RefObject<TransitioningView> | null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Transitioning.View
+      ref={ref}
+      transition={transition}
+      style={styles.container}
+    >
+      {data.map(
+        ({ backgroundColor, color, category, subCategories }, index) => {
+          return (
+            <TouchableNativeFeedback
+              key={category}
+              onPress={() => {
+                ref?.current?.animateNextTransition();
+                setCurrentIndex(index === currentIndex ? -1 : index);
+              }}
+              style={styles.cardContainer}
+            >
+              <View style={[styles.card, { backgroundColor }]}>
+                <Text style={[styles.heading, { color }]}>{category}</Text>
+                {index === currentIndex && (
+                  <View style={[styles.subCategories]}>
+                    {subCategories.map((subCategory) => {
+                      return (
+                        <Text
+                          key={subCategory}
+                          style={[styles.body, { color }]}
+                        >
+                          {subCategory}
+                        </Text>
+                      );
+                    })}
+                  </View>
+                )}
+              </View>
+            </TouchableNativeFeedback>
+          );
+        }
+      )}
       <StatusBar style="auto" />
-    </View>
+    </Transitioning.View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  cardContainer: {
+    flexGrow: 1,
+  },
+  card: {
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    fontSize: 38,
+    fontWeight: "700",
+    textTransform: "uppercase",
+    letterSpacing: -2,
+  },
+  subCategories: {
+    marginTop: 20,
+  },
+  body: {
+    fontSize: 20,
+    lineHeight: 20 * 1.5,
+    textAlign: "center",
   },
 });
